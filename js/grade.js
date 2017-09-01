37d0339e2cb9b8d5a483cd69d9ef61837c695420
shway250 <jackson_duhon@yahoo.com>
2017-09-01 11:15:03 -0700

    blergle dergle dooog

---
+++ js/grade.js
@@ -0,0 +1,13 @@
+// ###grade.js Write a program that will print the letter grade, given a variable with a test score. Display either "A", "B", "C", "D", or "F", for an score that is an integer between 0 and 100.
+
+// Requirements
+
+// Your program should have a variable to store the letter grade (an integer between 0 and 100)
+// For the letter grades, you may use whatever grading scale you like
+// You must use a switch statement (hint, you may need to review and think about how the switch statement works)
+
+var grades = [99,45,100,80,70,60,50];
+
+for(var i = 0; i< grades.length; i++){
+  //heres where you put your switch statement
+}
